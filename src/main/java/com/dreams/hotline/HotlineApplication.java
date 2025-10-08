502235ab9bb7a0b006a3f0f391af0c68d52521da
CheunRamassy <cheunramassy.pr@gmail.com>
2025-10-08 19:07:48 +0400

    Initialisation du projet

---
+++ src/main/java/com/dreams/hotline/HotlineApplication.java
@@ -0,0 +1,13 @@
+package com.dreams.hotline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dreams/hotline/HotlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreams.hotline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
